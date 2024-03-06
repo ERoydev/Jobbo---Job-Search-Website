1ea1619fdfaf6eb18599677fce8d5a3998023dfc
Emil Roydev <e.roydev@gmail.com>
2024-03-05 23:10:17 -0800

    customization on post a job fields

--- frontend/src/components/Pages/PostJob/PostFormModels/PostFormModel1.jsx
+++ frontend/src/components/Pages/PostJob/PostFormModels/PostFormModel1.jsx
@@ -3,6 +3,9 @@ export default function PostFormModel1() {
         <div className="model1">
                 <div className="job-description">
                     <label htmlFor="description">Company Overview</label>
+                    <p className="info">
+                        Introduce your company, business plans, activities or provide a concise overview of when the company was founded and any significant milestones you've achieved.
+                    </p>
                     <textarea
                         name="text_company_overview"
                         id="description"
@@ -14,6 +17,9 @@ export default function PostFormModel1() {
 
                 <div className="job-description">
                     <label htmlFor="description">Key Responsibilities</label>
+                    <p className="info">
+                        Describe the most important duties and tasks associated with a particular role or position. The skills that you want from the next employee.
+                    </p>
                     <textarea
                         name="text_key_responsibilities"
                         id="description"
@@ -25,6 +31,9 @@ export default function PostFormModel1() {
 
                 <div className="job-description">
                     <label htmlFor="description">Qualifications</label>
+                    <p className="info">
+                        Describe the skills, experience, knowledge, and attributes necessary to be successful in a particular role or position.
+                    </p>
                     <textarea
                         name="text_qualifications"
                         id="description"
@@ -36,6 +45,7 @@ export default function PostFormModel1() {
 
                 <div className="job-description">
                     <label htmlFor="description">Preferred skills</label>
+                    <p className="info">Indicate desirable qualities, knowledge, or experience that would be a plus for a role or position, but aren't necessarily essential qualifications. </p>
                     <textarea
                         name="text_preferred_skills"
                         id="description"
